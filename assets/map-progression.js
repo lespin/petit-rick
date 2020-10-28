13d25085d4492d179c9032637b433187d2504c0d
lespin <jeanpitouconsulting@free.fr>
2020-10-28 15:52:17 +0100

    map progression.js to assets/

---
+++ assets/map-progression.js
@@ -0,0 +1,6 @@
+export const MapProgression = {
+    'map1' : [ 'map2', 'map3' ],
+    'map2' : [ 'map4' ],
+    'map3' : [ 'map4','map5','map6' ],
+    'map4' : [ 'map5','map6','map7' ],
+}
--- src/unlocks.js
+++ src/unlocks.js
@@ -1,13 +1,16 @@
 import { History} from './persist.js'
+import { MapProgression } from '../assets/map-progression.js'
+
 const historyStore = History()
 
 
-const byMaps = {
+const byMaps = MapProgression
+/*const byMaps ={
     'map1' : [ 'map2', 'map3' ],
     'map2' : [ 'map4' ],
     'map3' : [ 'map4','map5','map6' ],
     'map4' : [ 'map5','map6','map7' ],
-}
+}*/
 function getUnlockedBy( fromMap ){
     return byMaps[ fromMap ] || []
 }
