4facb7bfc50b64c1a14b468f936302a7fc40fb90
lespin <jeanpitouconsulting@free.fr>
2020-10-28 15:38:55 +0100

    remove unused worklet related files

--- src/lib/MyWorkletNode.js
+++
@@ -1,7 +0,0 @@
-// MyWorkletNode.js
-export default class MyWorkletNode extends AudioWorkletNode {
-   constructor(context) {
-    super(context, 'my-worklet-processor')
-    console.log(this.channelCount)
-  }
-}
