c6544a708fb2a7710f8c61dba78bcb412bae8d12
lespin <jeanpitouconsulting@free.fr>
2020-10-29 17:07:20 +0100

    possible progression

--- assets/map-progression.js
+++ assets/map-progression.js
@@ -1,4 +1,4 @@
-export const InitialMap =  'map14'
+export const InitialMap =  'map1'
 export const MapProgression = {
     'map1' : [ 'map2' ],
     'map2' : [ 'map3', 'map4' ],
@@ -6,6 +6,7 @@ export const MapProgression = {
     'map4' : [ 'map5', 'map6' ],
     'map5' : [ 'map7' ],
     'map6' : [ 'map7' ],
+    'map7' : [ 'map8' ],
     'map8' : [ 'map9', 'map10' ],
     'map9' : [ 'map10', 'map11'  ],
     'map10' : [ 'map11', 'map12'  ],
