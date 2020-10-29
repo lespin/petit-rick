2330eb5a4c21b58c383b68e32f49c2c2cb5284ed
lespin <jeanpitouconsulting@free.fr>
2020-10-29 12:20:20 +0100

    new map

--- assets/map-progression.js
+++ assets/map-progression.js
@@ -1,4 +1,4 @@
-export const InitialMap =  'map13'
+export const InitialMap =  'map14'
 export const MapProgression = {
     'map1' : [ 'map2' ],
     'map2' : [ 'map3', 'map4' ],
@@ -6,5 +6,12 @@ export const MapProgression = {
     'map4' : [ 'map5', 'map6' ],
     'map5' : [ 'map7' ],
     'map6' : [ 'map7' ],
-    'map8' : [ 'map8' ],
+    'map8' : [ 'map9', 'map10' ],
+    'map9' : [ 'map10', 'map11'  ],
+    'map10' : [ 'map11', 'map12'  ],
+    'map11' : [ 'map12', 'map13'  ],
+    'map12' : [ 'map13', 'map14'  ],
+    'map13' : [ 'map14', 'map15'  ],
+//    'map14' : [ 'map15', 'map11'  ],
+
 }
